5f363bee47a8774e43ca691f58b55498d093eef3
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2020-08-02 20:54:37 +0100

    Disable fleet storage

--- src/main/stellics/campaign/submarkets/StellicsStorage.java
+++ src/main/stellics/campaign/submarkets/StellicsStorage.java
@@ -39,6 +39,11 @@ public class StellicsStorage extends StoragePlugin {
     }
 
     @Override
+    public boolean showInFleetScreen() {
+        return false;
+    }
+
+    @Override
     protected void createTooltipAfterDescription(TooltipMakerAPI tooltip, boolean expanded) {
     }
 }
