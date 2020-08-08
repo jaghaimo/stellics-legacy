0bf031a520f0d0fc103735f1a8e79e84ed00ea32
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2020-08-08 15:38:01 +0100

    Update project env

--- util/fetch-deps.sh
+++ util/fetch-deps.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash
 
 set -eu
-cd ..
 
-if [ ! -d "Console Commands" ]; then
+# Console Commands
+if [ ! -f "libs/lw_Console.jar" ]; then
+    tmp_dir=$(mktemp -d -t libs-cc-XXXXXXXXXX)
+    pushd $tmp_dir
     wget https://github.com/LazyWizard/console-commands/releases/download/dev-2020-07-12/Console_Commands_3.0_dev_2020-07-12.zip
-    unzip Console_Commands*
-    rm -f Console_Commands*.zip
+    unzip *
+    popd
+    cp $tmp_dir/*/jars/lw_Console.jar libs/
 fi
 
-if [ ! -d "LazyLib" ]; then
+# LazyLib
+if [ ! -f "libs/LazyLib.jar" ]; then
+    tmp_dir=$(mktemp -d -t libs-ll-XXXXXXXXXX)
+    pushd $tmp_dir
     wget https://github.com/LazyWizard/lazylib/releases/download/2.4f/LazyLib_2.4f.zip
-    unzip LazyLib*
-    rm -f LazyLib*.zip
+    unzip *
+    popd
+    cp $tmp_dir/*/jars/LazyLib.jar libs/
+fi
+
+# Starsector
+if [ ! -f "libs/starfarer.api.jar" ]; then
+    tmp_dir=$(mktemp -d -t libs-ss-XXXXXXXXXX)
+    pushd $tmp_dir
+    wget https://s3.amazonaws.com/fractalsoftworks/starsector/starsector_linux-0.9.1a-RC8.zip
+    unzip *
+    popd
+    cp $tmp_dir/*/starfarer.api.jar libs/
 fi
