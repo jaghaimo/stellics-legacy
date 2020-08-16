5d1e341c5cb95f527eeabe4e4d2355e880c5379d
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2020-08-16 21:02:39 +0100

    Fix same-system market distance calculation

--- src/main/stellics/campaign/econ/MarketComparator.java
+++ src/main/stellics/campaign/econ/MarketComparator.java
@@ -2,27 +2,47 @@ package stellics.campaign.econ;
 
 import java.util.Comparator;
 
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.util.Misc;
 
 import org.lwjgl.util.vector.Vector2f;
 
 /**
- * Compares two markets based on the distance to player.
+ * Compares two markets based on the distance to player fleet.
  */
 public class MarketComparator implements Comparator<MarketAPI> {
 
-    @Override
-    public int compare(MarketAPI m1, MarketAPI m2) {
-       return compare(
-                m1.getPrimaryEntity().getLocationInHyperspace(),
-                m2.getPrimaryEntity().getLocationInHyperspace()
-                );
+    CampaignFleetAPI playerFleet;
+
+    public MarketComparator() {
+        playerFleet = Global.getSector().getPlayerFleet();
     }
 
-    private int compare(Vector2f m1, Vector2f m2) {
-        float mDistance1 = Misc.getDistanceToPlayerLY(m1);
-        float mDistance2 = Misc.getDistanceToPlayerLY(m2);
+    @Override
+    public int compare(MarketAPI m1, MarketAPI m2) {
+        int comparison = compare(
+                m1.getPrimaryEntity().getLocationInHyperspace(),
+                m2.getPrimaryEntity().getLocationInHyperspace(),
+                playerFleet.getLocationInHyperspace()
+                );
+
+        // same system, use local coordinates
+        if (comparison == 0) {
+            comparison = compare(
+                    m1.getPrimaryEntity().getLocation(),
+                    m2.getPrimaryEntity().getLocation(),
+                    playerFleet.getLocation()
+                    );
+        }
+
+        return comparison;
+    }
+
+    private int compare(Vector2f m1, Vector2f m2, Vector2f p) {
+        float mDistance1 = Misc.getDistance(m1, p);
+        float mDistance2 = Misc.getDistance(m2, p);
 
         return (int) (mDistance1 - mDistance2);
     }
