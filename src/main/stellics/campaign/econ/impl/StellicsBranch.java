6b53f306828006fec83ec82b905010489b91055e
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2020-09-30 23:08:54 +0100

    Create codeql-analysis.yml

--- src/main/stellics/campaign/econ/impl/StellicsBranch.java
+++
@@ -1,7 +0,0 @@
-package stellics.campaign.econ.impl;
-
-import stellics.campaign.econ.BranchIndustry;
-
-@Deprecated(since = "0.9.1", forRemoval = true)
-public class StellicsBranch extends BranchIndustry {
-}
--- src/main/stellics/campaign/submarkets/StellicsStorage.java
+++
@@ -1,7 +0,0 @@
-package stellics.campaign.submarkets;
-
-import stellics.campaign.econ.StorageSubmarket;
-
-@Deprecated(since = "0.9.1", forRemoval = true)
-public class StellicsStorage extends StorageSubmarket {
-}
